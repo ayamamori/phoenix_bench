7965d3ed47d593a346c4034ede4454369c69dad2
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-07-26 13:53:24 +0900

    Implemented benchmark code

--- lib/phoenix_bench.ex
+++ lib/phoenix_bench.ex
@@ -1,24 +1,62 @@
 defmodule PhoenixBench do
 
   def bench do
-    join = Msgpax.pack!(%{topic: "rooms:lobby", event: "phx_join", ref: 1, payload: nil})|> IO.iodata_to_binary() 
+    chat_msgpack = Msgpax.pack!(%{topic: "rooms:lobby", event: "new_msg", ref: 2, payload: %{content: "aaa"}})|> IO.iodata_to_binary() 
     n_client = 1000
-    spawn_clients(join,n_client)
-  end
-  def spawn_clients(join_msgpack,n_client) when n_client>=1 do
-    spawn fn -> 
-      start_time = :os.system_time(:milli_seconds)
-      socket = Socket.Web.connect! "", 4000, path: "/socket/websocket?user_name=#{n_client}"
-      end_time = :os.system_time(:milli_seconds)
-      socket |> (Socket.Web.send! {:binary, join_msgpack})
-      socket |> Socket.Web.recv! |> elem(1) |> Msgpax.unpack! 
-      IO.puts (end_time - start_time)
+    [one_client | other_client] = create_clients(n_client)
+    #send one_client, {:send, chat_msgpack}
 
+    #clients= create_clients(n_client)
+    #clients |> Enum.each(&(&1 |> send({:send, chat_msgpack})))
+
+    #main_loop
+
+  end
+  def create_clients(n_client) when n_client>=1 do
+    join_msgpack = Msgpax.pack!(%{topic: "rooms:lobby", event: "phx_join", ref: 1, payload: nil})|> IO.iodata_to_binary() 
+
+    receive_pid = spawn (fn -> receive_loop end)
+    (for x <- 1..n_client, do: x)
+      |> Enum.map(fn user_name -> 
+            spawn (fn -> join_channel(user_name, join_msgpack, receive_pid) end)
+         end)
+  end
+
+  def join_channel(user_name, join_msgpack, receive_pid) do
+    start_time = :os.system_time(:milli_seconds)
+
+    socket = Socket.Web.connect! "localhost", 4000, path: "/socket/websocket?user_name=#{user_name}"
+    socket |> (Socket.Web.send! {:binary, join_msgpack})
+    socket |> Socket.Web.recv! |> elem(1) |> Msgpax.unpack! 
+
+    end_time = :os.system_time(:milli_seconds)
+    IO.puts "Handshake time: #{(end_time - start_time)}[ms]"
+    spawn fn -> recv_loop(socket, receive_pid) end
+    send_loop(socket)
+  end
+
+  def send_loop(socket) do
+    receive do
+      {:send, msgpack} -> 
+        socket |> (Socket.Web.send! {:binary, msgpack})
     end
-    
-    spawn_clients(join_msgpack,n_client-1)
+    send_loop(socket)
   end
-  def spawn_clients(join_msgpack,n_client) when n_client<1 do
-    spawn_clients(join_msgpack,0)
+
+  def recv_loop(socket, receive_pid) do
+    received = socket 
+      |> Socket.Web.recv! 
+      |> elem(1) 
+      |> Msgpax.unpack! 
+    send receive_pid, {:receive, received}
+    recv_loop(socket, receive_pid)
   end
+
+  def receive_loop do
+    receive do
+      {:receive, received} -> IO.puts :os.system_time(:milli_seconds) 
+    end
+    receive_loop
+  end
+
 end
