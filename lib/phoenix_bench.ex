3315b29c96c58bc321e72be0f11f2695c88a668d
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-08-19 12:56:04 +0900

    Update

--- lib/phoenix_bench.ex
+++ lib/phoenix_bench.ex
@@ -15,54 +15,50 @@ defmodule PhoenixBench do
 
   def bench do
     chat_msgpack = Msgpax.pack!(%{
-      Event: "new_msg", 
-      Data: Msgpax.pack!(%{
+        Event: "msg:new", 
         Topic: "rooms:lobby", 
         Ref: 2, 
-        Payload: %{Content: "aaa"}
-        })|> IO.iodata_to_binary()
+        Content: "aaa",
       })|> IO.iodata_to_binary() 
-    n_client = 10
-    host_name = "localhost"
-    [one_client | other_client] = create_clients(n_client, host_name)
-    send one_client, {:send, chat_msgpack}
+    n_client = 1
+    host_name = "104.199.139.193"
+    #host_name = "localhost"
+    #[one_client | other_client] = create_clients(n_client, host_name)
+    #send one_client, {:send, chat_msgpack}
 
-    #clients= create_clients(host_name, n_client)
-    #clients |> Enum.each(&(&1 |> send({:send, chat_msgpack})))
+    clients= create_clients(n_client, host_name)
+    clients |> Enum.each(&(&1 |> send({:send, chat_msgpack})))
 
     receive_loop
 
   end
 
   @doc """
-    Create clients with random (sequencial number) user_name
+    Create clients with random (sequencial number) user_id
   """
   def create_clients(n_client, host_name, channel \\ "rooms:lobby") when n_client>=1 do
     receive_pid = spawn (fn -> receive_loop end)
     (for x <- 1..n_client, do: x)
-      |> Enum.map(fn user_name -> 
-            create_client(user_name, host_name, channel, receive_pid)
+      |> Enum.map(fn user_id -> 
+            create_client(user_id, host_name, channel, receive_pid)
          end)
   end
 
-  def create_client(user_name, host_name, channel \\ "rooms:lobby") when is_binary(channel) do
-    create_client(user_name, host_name, channel, (spawn (fn -> receive_loop end)))
+  def create_client(user_id, host_name, channel \\ "rooms:lobby") when is_binary(channel) do
+    create_client(user_id, host_name, channel, (spawn (fn -> receive_loop end)))
   end
-  defp create_client(user_name, host_name, channel, receive_pid) do
+  defp create_client(user_id, host_name, channel, receive_pid) do
     join_msgpack = Msgpax.pack!(%{
         Event: "phx_join", 
-        Data: Msgpax.pack!(%{
-          Topic: "#{channel}", 
-          Ref: 1, 
-          Payload: nil
-          })|> IO.iodata_to_binary()
-        })|> IO.iodata_to_binary() 
-    spawn (fn -> join_channel(host_name, user_name, join_msgpack, receive_pid) end)
+        Topic: "#{channel}", 
+        Ref: 1, 
+      })|> IO.iodata_to_binary() 
+    spawn (fn -> join_channel(host_name, user_id, join_msgpack, receive_pid) end)
   end
 
-  def join_channel(host_name, user_name, join_msgpack, receive_pid) do
+  def join_channel(host_name, user_id, join_msgpack, receive_pid) do
 
-    socket = Socket.Web.connect! host_name, 4000, path: "/socket/websocket?user_name=#{user_name}"
+    socket = Socket.Web.connect! host_name, 4000, path: "/socket/websocket?user_id=#{user_id}&user_name=aaa"
     socket |> (Socket.Web.send! {:binary, join_msgpack})
     socket |> Socket.Web.recv! |> elem(1) |> Msgpax.unpack! 
 
@@ -83,9 +79,7 @@ defmodule PhoenixBench do
       |> Socket.Web.recv! 
       |> elem(1) 
       |> Msgpax.unpack! 
-    event = received["Event"] |> IO.inspect
-    data = received["Data"] |> Msgpax.unpack!
-    send receive_pid, {:receive, %{Event: event, Data: data}}
+    send receive_pid, {:receive, received}
     recv_loop(socket, receive_pid)
   end
 
